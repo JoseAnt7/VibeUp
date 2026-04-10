1c71a3164b8d5031fbca5011bc1614a665835c62
HackPressYT <hackpress78@gmail.com>
2026-04-10 16:13:54 +0200

    ajuste_puerto_render

--- backend/app.py
+++ backend/app.py
@@ -82,4 +82,4 @@ def _ensure_sqlite_song_metrics_columns():
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
